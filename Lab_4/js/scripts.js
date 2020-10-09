97f5e2266e1fb56976a20d4d8e082a59fcf4d6be
Brandon Cantey <bcantey2@montgomerycollege.edu>
2020-10-08 21:56:01 -0400

    update

--- Lab_4/js/scripts.js
+++ Lab_4/js/scripts.js
@@ -1,50 +1,51 @@
 let myFullName = "Brandon Cantey" // 10 characters in name...not including space
 // 1 Change the content of the div with class "header" to "[Your name]'s Lab 4"
 document.querySelector(".header").innerHTML = myFullName + "\'s Lab 4\n";
-
+ 
 // 2 Insert two paragraphs into the div with the class "content"
 //  Label each paragraph with a distinct class name
-var paragraphone = document.createElement("paragraph1");
-var paragraphtwo = document.createElement("paragraph2");
-var paragraphonetext = document.createTextNode("paragraph1");
-var paragraphtwotext = document.createTextNode("paragraph2");
-paragraphone.appendChild(paragraphonetext);
-paragraphtwo.appendChild(paragrphtwotext);
-var newcontentparagraphs = document.querySelector(".content");
-newcontentparagraphs.appendChild(paragraphone);
-newcontentparagraphs.appendChild(paragraphtwo);
-paragraphone.className = "content1"
-paragraphtwo.className = "content2"
-
+var paragraphOne = document.createElement("Paragraph1:");
+var paragraphTwo = document.createElement("Paragraph2:");
+var paragraphOneText = document.createTextNode("Paragraph1:");
+var paragraphTwoText = document.createTextNode("Paragraph2:");
+paragraphOne.appendChild(paragraphOneText);
+paragraphTwo.appendChild(paragraphTwoText);
+var newContentParagraphs = document.querySelector(".content");
+newContentParagraphs.appendChild(paragraphOne);
+newContentParagraphs.appendChild(paragraphTwo);
+paragraphOne.className = "content1"
+paragraphTwo.className = "content2"
+ 
 // 3 Into the first paragraph, insert the phrase "my name has " length of your name " characters"
 //      (e.g. my name has 10 characters).
-
-var paragraph = "my name has " + myFullName.length + " characters.";
-paragraphone.append(paragraph);
-
+ 
+var paragraph = " my name has " + myFullName.length + " characters.";
+paragraphOne.append(paragraph);
+ 
 // 4 & 5 Into the second paragraph tag, return the 3rd character in your first name
-
-var three = " the third character is " + myFullName.charAt(2);
-paragraphtwo.append(three);
-
+ 
+var third = " the third charater is " + myFullName.charAt(2);
+paragraphTwo.append(third);
+ 
 // 6 Add a new line to your second paragraph
-
-paragraphtwo.append("\n");
-
+ 
+paragraphTwo.append("\n");
+ 
 // 7 Return the final three characters of your last name to that new line
-
-var fnal = ", the third characters of my last name is " + myFullName.substring(myFullName.length - 3, myFullName.length);
-paragraphtwo.append(final);
-
+ 
+var final = ", the third charaters of my last name is  " + myFullName.substring(myFullName.length - 3, myFullName.length);
+paragraphTwo.append(final);
+ 
+ 
 // 8 Substring your first and last name into two separate variables
-
-var first = myFullName.substring (0,5);
-var last = myFullName.substring(7,13);
-
+ 
+var first = myFullName.substring (0,6)
+var last = myFullName.substring(8,13)
+ 
 // 9 Add the total length of your first and last names together
-
-var total = first.length +last.length;
-
+ 
+var total = first.length + last.length;
+ 
 // 10 Display that total next to your name in your header
 
 document.querySelector(".header").append("The length of my name is: " + total);
